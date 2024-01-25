2cf9e15f4fa20da9fd12c4cd72e3b5e4642c5b20
Kevin David Topon Salazar <kdtopon@espe.edu.ec>
2024-01-24 19:43:16 -0500

    prueba_inter

---
+++ database/adopters.js
@@ -0,0 +1,17 @@
+// database/adopters.js
+module.exports = [
+    { "id": 1, 
+    "name": "Saveedra",
+     "address": "123 Main St" 
+    },
+
+    { "id": 2, 
+    "name": "Tamayo",
+     "address": "456 Oak St" 
+    },
+    
+    { "id": 3, 
+    "name": "Tavaris",
+     "address": "789 Pine St" 
+    }
+];
---
+++ database/dogs.js
@@ -0,0 +1,14 @@
+// database/dogs.js
+module.exports = [
+    { "id": 1,
+     "name": "EDO" 
+    },
+
+    { "id": 2, 
+    "name": "Sistemas Operativos"
+    },
+
+    { "id": 3,
+     "name": "Seguridad Informatica" 
+    }
+];
